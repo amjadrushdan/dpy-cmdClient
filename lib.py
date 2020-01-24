123a0c18c9edeee3464826dac03cbbb6b64372f4
Interitio <cona@thewisewolf.dev>
2020-01-25 07:59:59 +1000

    (Lib): New lib utility `sterilise_content`.

--- lib.py
+++ lib.py
@@ -18,3 +18,26 @@ class InvalidContext(Exception):
     Throw when the context available doesn't match the context expected.
     """
     pass
+
+
+def sterilise_content(content):
+    """
+    Sterilse everyone and here mentions in the provided string.
+    Specifically, adds a zer width space after the `@` symbol
+    when such a ping is detected.
+
+    Parameters
+    ----------
+    content: str
+        String to sterilise
+
+    Returns: str
+        Sterilsed string.
+    """
+    content = content.replace("@everyone", "@​everyone")
+    content = content.replace("@here", "@​here")
+    asciimsg = content.encode('ascii', errors='ignore').decode()
+    if "@everyone" in asciimsg or "@here" in asciimsg:
+        content = content.replace("@", "@​")
+
+    return content
